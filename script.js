cb720892435753c225f65e9b6e81b2159817f067
unknown <nibeshmaharjan17@gmail.com>
2026-01-26 14:05:45 +0545

    lightbox popup

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+    });
+});
+
+// Contact form message
 const form = document.getElementById("contactForm");
 const msg = document.getElementById("msg");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     msg.textContent = "Thank you! Your message has been sent.";
-    msg.style.color = "#6c63ff";
+    msg.style.color = "#8e2de2";
     form.reset();
+});
+// LIGHTBOX POPUP
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+document.querySelectorAll('.graphic-card img').forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+        lightboxImg.alt = img.alt;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// Close when clicking outside image
+lightbox.addEventListener('click', e => {
+    if(e.target === lightbox) {
+        lightbox.style.display = 'none';
+    }
 });
